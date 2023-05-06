170fd582e7db53fc3c51266d4e9ad36631de5f2f
Fran Moreno <fran.moreno.se@gmail.com>
2023-05-06 10:17:11 +0200

    Created short productivity tracker

---
+++ README.md
@@ -0,0 +1,10 @@
+# Productivity Tracker
+
+![Sample graph](src/sample.png "Sample Graph")
+
+Short script to execute while you're on your daily work routine to keep track
+of your productivity based on the time you spent on different actions.
+
+Sample actions given are: PROJECT, COURSE, HOUSE, REST
+
+Easy to execute from prompt.
---
+++ track.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import os
+
+from csv import DictWriter, reader
+from datetime import date, timedelta
+from enum import Enum
+from time import time
+from typing import Tuple, Optional
+
+LOGS_FOLDER = "daily_logs"
+
+
+class ACTION(Enum):
+    PROJECT = 0
+    COURSE = 1
+    HOUSE = 2  # Involves: cat, householding chores, shopping, cooking, etc.
+    REST = 3
+
+
+def start(tag: int) -> Optional[str]:
+    try:
+        return ACTION(tag).name
+    except ValueError:
+        return None
+
+
+def stop() -> float:
+    input("Write something to finish.")
+    return time()
+
+
+def track_performance() -> Tuple[str, int]:
+    options = ' | '.join(f'{key}: {value}' for key, value in zip([e.name for e in ACTION], [e.value for e in ACTION]))
+    print(options)
+
+    task = None
+    while not task:
+        choice = int(input("-> "))
+        task = start(choice)
+        if not task:
+            print("Not valid. Try again.")
+
+    time_start = time()
+    input("Type anything to finish: ")
+    interval = round(time() - time_start)
+    return task, interval
+
+
+def save_action(file: str, action: str, interval: int) -> None:
+    with open(file, "a+", newline='') as f:
+        fieldnames = ["action", "interval"]
+        writer = DictWriter(f, fieldnames=fieldnames)
+        writer.writerow({"action": action, "interval": interval})
+
+
+def compute_totals(file: str) -> dict:
+    records = dict()
+    with open(file, "r") as f:
+        csv_reader = reader(f, delimiter=",")
+        for row in csv_reader:
+            records[row[0]] = int(row[1]) + records.get(row[0], 0)
+
+    print("Time per action")
+    for action in records:
+        print(f"{action:7}\t\t", str(timedelta(seconds=records[action])), "h")
+
+    return records
+
+
+def show_results(records: dict):
+    fig, ax = plt.subplots()
+    ax.pie(records.values(), labels=records.keys(), autopct='%1.1f%%')
+    plt.title(str(date.today()))
+    plt.show()
+
+
+if __name__ == "__main__":
+    current_date = str(date.today())
+
+    if not os.path.exists(LOGS_FOLDER):
+        os.makedirs(LOGS_FOLDER)
+
+    filename = f"{LOGS_FOLDER}/{current_date}.csv"
+
+    finish = False
+    while not finish:
+        task, time_spent = track_performance()
+        save_action(filename, task, time_spent)
+
+        i = input("Keep tracking? [y/n]")
+        finish = True if i == "n" else False
+
+    totals = compute_totals(filename)
+    show_results(totals)
+
+
+
+
+
+
